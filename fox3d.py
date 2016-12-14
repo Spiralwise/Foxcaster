ecab43ddf00de7132330c2ae8adc11ace5250f53
Spiralwise <sebastien.poulmane@gmail.com>
2016-12-14 22:47:54 +0100

    Can move player + archaic shadow

--- fox3d.py
+++ fox3d.py
@@ -6,11 +6,14 @@
 # Spiralwise - 2016
 #
 # This raycaster is inspired by http://lodev.org/cgtutor/raycasting.html
+#
+# TODO : Find a vector library for a more efficient vector manipulation
 ######################
 
 import pygame
 from pygame.locals import *
-from math import inf, sqrt
+from math import *
+from time import perf_counter
 
 DUMMY_MAP = [
 	[1, 1, 1, 1, 1, 1],
@@ -30,6 +33,7 @@ COLOR = {
 	'BLACK': (0, 0, 0),
 	'WHITE': (255, 255, 255),
 	'WALL': (196, 196, 0),
+	'SHADOW_WALL': (96, 96, 0),
 	'CEIL': (0, 127, 127),
 	'FLOOR': (96, 96, 127)
 }
@@ -42,13 +46,32 @@ screen = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption("Foxenstein 3D")
 
 Player = {
-	'pos': [BLOCK_SIZE*2, BLOCK_SIZE*2],
-	'dir': [0.0, -1.0]
+	'pos': [BLOCK_SIZE*3, BLOCK_SIZE*3],
+	'dir': [0.0, -1.0],
+	'speed': 1.0,
+	'rot_speed': 0.01,
+	'state': "STAY"
 }
 
 Camera = {
 	'dir': [0.66, 0]
 }
+
+Font = pygame.font.SysFont("monospace", 10)
+
+def display_framerate(framerate):
+	text = Font.render(str(framerate), 1, (28, 28, 50))
+	screen.blit(text, (10, 10))
+
+
+def rotate_vector(vec, angle):
+	rot_mtx = [
+		[cos(angle), -sin(angle)],
+		[sin(angle), cos(angle)]
+	]
+	vx = rot_mtx[0][0] * vec[0] + rot_mtx[0][1] * vec[1]
+	vy = rot_mtx[1][0] * vec[0] + rot_mtx[1][1] * vec[1]
+	return [vx, vy]
 
 
 # Cast a ray to the nearest wall and returns the length
@@ -113,30 +136,68 @@ def hit_wall(x):
 	else:
 		distance = sideHitDist[0]
 
-	return distance
+	return distance, side
 
 
+# input x: vertical strip on screen
+# input distance: distance to the nearest wall through the vertical strip
 def draw_wall(x, distance):
-	perceivedHeight = int(WALL_HEIGHT/distance)
+	perceivedHeight = int(WALL_HEIGHT/distance[0])
+	if distance[1]:
+		color = COLOR['SHADOW_WALL']
+	else:
+		color = COLOR['WALL']
 	pygame.draw.line(screen,
-		COLOR['WALL'],
+		color,
 		(x, (perceivedHeight + SCREEN_SIZE[1]/2)),
 		(x, (-perceivedHeight + SCREEN_SIZE[1]/2)))
 
 
+def process_player(delta_time):
+	if Player['state'] is "FWD":
+		Player['pos'][0] += Player['dir'][0] * Player['speed']*delta_time
+		Player['pos'][1] += Player['dir'][1] * Player['speed']*delta_time
+	elif Player['state'] is "BKWD":
+		Player['pos'][0] -= Player['dir'][0] * Player['speed']*delta_time
+		Player['pos'][1] -= Player['dir'][1] * Player['speed']*delta_time
+	elif Player['state'] is "TURN_L":
+		Player['dir'] = rotate_vector(Player['dir'], -Player['rot_speed'])
+	elif Player['state'] is "TURN_R":
+		Player['dir'] = rotate_vector(Player['dir'], Player['rot_speed'])
+
+# ---- MAIN
+cur_time = perf_counter()
 running = True
 while running:
+	# ---- Draw
 	screen.fill(COLOR['BLACK'])
 	pygame.draw.rect(screen, COLOR['CEIL'], (1, 0, SCREEN_SIZE[0], SCREEN_SIZE[1]/2))
 	pygame.draw.rect(screen, COLOR['FLOOR'], (0, SCREEN_SIZE[1]/2, SCREEN_SIZE[0], SCREEN_SIZE[1]/2))
 	for col in range(SCREEN_SIZE[0]):
 		draw_wall(col, hit_wall(col))
 
+	last_time = cur_time
+	cur_time = perf_counter()
+	frame_duration = cur_time - last_time
+	framerate = 1.0/frame_duration
+	display_framerate(framerate)
 	pygame.display.flip()
 
+	# ---- Input
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			break
 		elif event.type == KEYDOWN:
 			if event.key == K_ESCAPE:
 				running = False
+			elif event.key == K_w:
+				Player['state'] = "FWD"
+			elif event.key == K_s:
+				Player['state'] = "BKWD"
+			elif event.key == K_a:
+				Player['state'] = "TURN_L"
+			elif event.key == K_d:
+				Player['state'] = "TURN_R"
+		elif event.type == KEYUP:
+			Player['state'] = "STAY"
+	process_player(last_time - cur_time)
